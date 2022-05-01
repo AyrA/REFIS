b7aa8d3c10fed6db433ed9e508d6ceeca55fba2b
Kevin Gut <kevin.gut@bitm.sg>
2022-05-01 11:16:35 +0200

    Document index main file

--- REFIS/RefisIndex.cs
+++ REFIS/RefisIndex.cs
@@ -6,20 +6,39 @@ using System.Threading.Tasks;
 
 namespace REFIS
 {
+    /// <summary>
+    /// REFIS header index from a dump
+    /// </summary>
     public class RefisIndex
     {
+        /// <summary>
+        /// REFIS files
+        /// </summary>
         public readonly Dictionary<Guid, RefisIndexEntry> Files;
 
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public RefisIndex()
         {
             Files = new Dictionary<Guid, RefisIndexEntry>();
         }
 
+        /// <summary>
+        /// Gets all file ids
+        /// </summary>
+        /// <returns>File ids</returns>
+        /// <remarks>This also returns ids of incomplete files</remarks>
         public Guid[] GetIds()
         {
             return Files.Keys.ToArray();
         }
 
+        /// <summary>
+        /// Adds a header to the index
+        /// </summary>
+        /// <param name="H">Header</param>
+        /// <param name="Offset">Offset in dump file of the header</param>
         public void AddHeader(RefisHeader H, long Offset)
         {
             if (H == null)
