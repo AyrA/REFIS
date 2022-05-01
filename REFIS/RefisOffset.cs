606512433ac8d4b50c352e75b5e8cf8bcb7a0be0
Kevin Gut <kevin.gut@bitm.sg>
2022-05-01 11:20:16 +0200

    Document index entry type

--- REFIS/RefisOffset.cs
+++ REFIS/RefisOffset.cs
@@ -2,11 +2,25 @@
 
 namespace REFIS
 {
+    /// <summary>
+    /// Represents a header with file offset
+    /// </summary>
     public class RefisOffset
     {
+        /// <summary>
+        /// REFIS header
+        /// </summary>
         public readonly long Offset;
+        /// <summary>
+        /// Offset of the header in the dump file
+        /// </summary>
         public readonly RefisHeader Header;
 
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="header">Header</param>
+        /// <param name="offset">Offset of the header in the dump file</param>
         public RefisOffset(RefisHeader header, long offset)
         {
             Offset = offset;
